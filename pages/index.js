7f6558695f5797c1cf94ecd5702657c679dbed8c
waleedrana777 <waleedrana777@example.com>
2022-05-05 13:46:04 +0500

    2

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Table from "../components/Table.jsx";
 import React from "react";
-import Accordian from "../components/Accordian";
+import MyAccordian from "../components/MyAccordian.jsx";
 
 export default function Home() {
 	return (
 		<React.Fragment>
 			<Table />
-			<Accordian />
+			<div className='m-4'>
+				<MyAccordian />
+			</div>
 		</React.Fragment>
 	);
 }
